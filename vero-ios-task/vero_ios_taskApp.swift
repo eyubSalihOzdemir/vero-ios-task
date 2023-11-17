52498da3257d2316481de3ce578d2aded6b829f7
eyubSalihOzdemir <eyupsalih776@gmail.com>
2023-11-17 15:14:51 +0300

    Initial Commit

---
+++ vero-ios-task/vero_ios_taskApp.swift
@@ -0,0 +1,17 @@
+//
+//  vero_ios_taskApp.swift
+//  vero-ios-task
+//
+//  Created by Salih Özdemir on 17.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct vero_ios_taskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
