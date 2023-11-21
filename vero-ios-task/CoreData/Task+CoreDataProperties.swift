1ae7195c01bbecfa893aaf27726da0810c940d89
eyubSalihOzdemir <eyupsalih776@gmail.com>
2023-11-21 16:53:24 +0300

    added wrappers to task core data class for ease of use. it will also be easier to make changes in the future

--- vero-ios-task/CoreData/Task+CoreDataProperties.swift
+++ vero-ios-task/CoreData/Task+CoreDataProperties.swift
@@ -30,19 +30,33 @@ extension Task {
     @NSManaged public var workingTime: String?
     
     enum CodingKeys: String, CodingKey {
-        case businessUnit = "businessUnit"
-        case businessUnitKey = "businessUnitKey"
-        case colorCode = "colorCode"
-        case isAvailableInTimeTrackingKioskMode = "isAvailableInTimeTrackingKioskMode"
-        case parentTaskID = "parentTaskID"
-        case preplanningBoardQuickSelect = "preplanningBoardQuickSelect"
-        case sort = "sort"
+        case title = "title"
         case task = "task"
         case taskDescription = "description"
-        case title = "title"
+        case sort = "sort"
         case wageType = "wageType"
+        case businessUnitKey = "businessUnitKey"
+        case businessUnit = "businessUnit"
+        case parentTaskID = "parentTaskID"
+        case preplanningBoardQuickSelect = "preplanningBoardQuickSelect"
+        case colorCode = "colorCode"
         case workingTime = "workingTime"
+        case isAvailableInTimeTrackingKioskMode = "isAvailableInTimeTrackingKioskMode"
     }
+    
+    var wrappedTitle: String { title ?? "No title" }
+    var wrappedTask: String { task ?? "No task" }
+    var wrappedTaskDescription: String { taskDescription ?? "No description" }
+    var wrappedSort: String { sort ?? "No sort" }
+    var wrappedWageType: String { wageType ?? "No wage type" }
+    var wrappedBusinessUnitKey: String { businessUnitKey ?? "No business unit key" }
+    var wrappedBusinessUnit: String { businessUnit ?? "No business unit" }
+    var wrappedParentTaskID: String { parentTaskID ?? "No parent task ID" }
+    var wrappedPreplanningBoardQuickSelect: String { preplanningBoardQuickSelect ?? "No preplanning board quick select" }
+    var wrappedColorCode: String { colorCode ?? "#ffffff" }
+    var wrappedWorkingTime: String { workingTime ?? "No working time" }
+    var wrappedIsAvailableTimeTrackingKioskMode: Bool { isAvailableInTimeTrackingKioskMode }
+    
 }
 
 extension Task : Identifiable {
