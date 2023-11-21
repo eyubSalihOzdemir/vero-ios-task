ca9ce3563b18a960c3cfd4c07363f1074d5073db
eyubSalihOzdemir <eyupsalih776@gmail.com>
2023-11-21 16:54:44 +0300

    made search bar functional. it's now able to filter the core data results for title. I extracted the scroll view as a new view and I use it with a filter parameter. this allows the filter function to fire again when the search text changes.

--- vero-ios-task/Views/ContentView.swift
+++ vero-ios-task/Views/ContentView.swift
@@ -9,15 +9,12 @@ import SwiftUI
 import CoreData
 import RefreshableScrollView
 
-struct ContentView: View {
-    @Environment(\.managedObjectContext) var moc
-    
+struct ContentView: View {    
     @StateObject var contentViewViewModel = ContentViewViewModel()
     
-    @FetchRequest(sortDescriptors: []) var tasks: FetchedResults<Task>
-    
-    @State private var showingAlert = false
     @State private var searchText = ""
+    
+    //@FetchRequest(sortDescriptors: []) var tasks: FetchedResults<Task>
     
     init() {
         
@@ -26,90 +23,7 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             NavigationView {
-                VStack {
-                    if tasks.count == 0 {
-                        Text("No tasks here—quiet as a library.")
-                    } else {
-                        RefreshableScrollView {
-                            ForEach(tasks) { task in
-                                LazyVStack(alignment: .leading) {
-                                    ZStack {
-                                        Color.white
-                                    
-                                        HStack {
-                                            RoundedRectangle(cornerRadius: 16, style: .continuous)
-                                                .frame(width: 50, height: 50)
-                                                .foregroundStyle(Color.init(hex: task.colorCode ?? "#000000").opacity(0.2))
-                                                .overlay {
-                                                    RoundedRectangle(cornerRadius: 16, style: .continuous)
-                                                        .stroke(Color.init(hex: task.colorCode ?? "#000000").opacity(0.5), lineWidth: 2)
-                                                }
-                                                .overlay {
-                                                    Image(systemName: "pin.fill")
-                                                        .foregroundStyle(Color.init(hex: task.colorCode ?? "#000000"))
-                                                }
-                                            
-                                            VStack(alignment: .leading) {
-                                                Text(task.title ?? "No title")
-                                                    .font(.title2.weight(.semibold))
-                                                
-                                                Text("Task: \(task.task ?? "No task")")
-                                                    .font(.callout.weight(.regular))
-                                                
-                                                Text(task.taskDescription ?? "No description")
-                                                    .font(.callout.weight(.light))
-                                                    .padding(.top, 2)
-                                            }
-                                            Spacer()
-                                        }
-                                        .padding(10)
-                                    }
-                                    .fixedSize(horizontal: false, vertical: true)
-                                    .cornerRadius(20)
-                                    .padding(.horizontal)
-                                    .shadow(color: Color.black.opacity(0.2), radius: 2)
-                                }
-                            }
-                            .onDelete(perform: removeTask)
-                        }
-                        .refreshable {
-                            removeAllTasks()
-                            contentViewViewModel.fetch()
-                        }
-                    }
-                    
-                }
-                .navigationTitle("Tasks")
-                .toolbar {
-                    ToolbarItem(placement: .topBarLeading) {
-                        Button {
-                            removeAllTasks()
-                            contentViewViewModel.fetch()
-                        } label: {
-                            Text(tasks.count > 0 ? "Refresh" : "Get tasks")
-                        }
-                    }
-                    
-                    if tasks.count > 0 {
-                        ToolbarItem(placement: .topBarTrailing) {
-                            Button {
-                                showingAlert.toggle()
-                            } label: {
-                                Text("Clear")
-                            }
-                            .alert("Delete Everything", isPresented: $showingAlert) {
-                                Button("Cancel", role: .cancel) {
-                                    // do nothing
-                                }
-                                Button("Yes") {
-                                    removeAllTasks()
-                                }
-                            } message: {
-                                Text("Are you sure you want to delete all tasks?")
-                            }
-                        }
-                    }
-                }
+                FilteredResults(filter: searchText, contentViewViewModel: contentViewViewModel)
             }
             .searchable(text: $searchText, prompt: "Search for tasks")
             
@@ -128,25 +42,6 @@ struct ContentView: View {
             .animation(.easeInOut, value: contentViewViewModel.loading)
         }
         .ignoresSafeArea()
-    }
-    
-    func removeTask(at offsets: IndexSet) {
-        for index in offsets {
-            let task = tasks[index]
-            moc.delete(task)
-        }
-        PersistenceController.shared.save()
-    }
-    
-    func removeAllTasks() {
-        contentViewViewModel.loading = true
-        
-        for task in tasks {
-            moc.delete(task)
-        }
-        PersistenceController.shared.save()
-        
-        contentViewViewModel.loading = false
     }
 }
 
---
+++ vero-ios-task/Views/FilteredResults.swift
@@ -0,0 +1,141 @@
+//
+//  FilteredResults.swift
+//  vero-ios-task
+//
+//  Created by Salih Özdemir on 21.11.2023.
+//
+
+import SwiftUI
+import RefreshableScrollView
+
+struct FilteredResults: View {
+    @Environment(\.managedObjectContext) var moc
+    
+    @ObservedObject var contentViewViewModel: ContentViewViewModel
+    
+    @FetchRequest var tasks: FetchedResults<Task>
+    
+    @State private var showingAlert = false
+    
+    init(filter: String, contentViewViewModel: ContentViewViewModel) {
+        if filter.isEmpty {
+            _tasks = FetchRequest<Task>(sortDescriptors: [])
+        } else {
+            _tasks = FetchRequest<Task>(sortDescriptors: [], predicate: NSPredicate(format: "title BEGINSWITH %@", filter))
+        }
+        
+        self.contentViewViewModel = contentViewViewModel
+    }
+    
+    var body: some View {
+        VStack {
+            if tasks.count == 0 {
+                Text("No tasks here—quiet as a library.")
+            } else {
+                RefreshableScrollView {
+                    ForEach(tasks) { task in
+                        LazyVStack(alignment: .leading) {
+                            ZStack {
+                                Color.white
+                            
+                                HStack {
+                                    RoundedRectangle(cornerRadius: 16, style: .continuous)
+                                        .frame(width: 50, height: 50)
+                                        .foregroundStyle(Color.init(hex: task.colorCode ?? "#000000").opacity(0.2))
+                                        .overlay {
+                                            RoundedRectangle(cornerRadius: 16, style: .continuous)
+                                                .stroke(Color.init(hex: task.colorCode ?? "#000000").opacity(0.5), lineWidth: 2)
+                                        }
+                                        .overlay {
+                                            Image(systemName: "pin.fill")
+                                                .foregroundStyle(Color.init(hex: task.colorCode ?? "#000000"))
+                                        }
+                                    
+                                    VStack(alignment: .leading) {
+                                        Text(task.title ?? "No title")
+                                            .font(.title2.weight(.semibold))
+                                        
+                                        Text("Task: \(task.task ?? "No task")")
+                                            .font(.callout.weight(.regular))
+                                        
+                                        Text(task.taskDescription ?? "No description")
+                                            .font(.callout.weight(.light))
+                                            .padding(.top, 2)
+                                    }
+                                    Spacer()
+                                }
+                                .padding(10)
+                            }
+                            .fixedSize(horizontal: false, vertical: true)
+                            .cornerRadius(20)
+                            .padding(.horizontal)
+                            .shadow(color: Color.black.opacity(0.2), radius: 2)
+                        }
+                    }
+                    .onDelete(perform: removeTask)
+                }
+                .refreshable {
+                    removeAllTasks()
+                    contentViewViewModel.fetch()
+                }
+            }
+            
+        }
+        .navigationTitle("Tasks")
+        .toolbar {
+            ToolbarItem(placement: .topBarLeading) {
+                Button {
+                    removeAllTasks()
+                    contentViewViewModel.fetch()
+                } label: {
+                    Text(tasks.count > 0 ? "Refresh" : "Get tasks")
+                }
+            }
+            
+            if tasks.count > 0 {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        showingAlert.toggle()
+                    } label: {
+                        Text("Clear")
+                    }
+                    .alert("Delete Everything", isPresented: $showingAlert) {
+                        Button("Cancel", role: .cancel) {
+                            // do nothing
+                        }
+                        Button("Yes") {
+                            removeAllTasks()
+                        }
+                    } message: {
+                        Text("Are you sure you want to delete all tasks?")
+                    }
+                }
+            }
+        }
+    }
+    
+    func removeTask(at offsets: IndexSet) {
+        for index in offsets {
+            let task = tasks[index]
+            moc.delete(task)
+        }
+        PersistenceController.shared.save()
+    }
+
+    func removeAllTasks() {
+        contentViewViewModel.loading = true
+        
+        for task in tasks {
+            moc.delete(task)
+        }
+        PersistenceController.shared.save()
+        
+        contentViewViewModel.loading = false
+    }
+}
+
+
+
+#Preview {
+    FilteredResults(filter: "G", contentViewViewModel: ContentViewViewModel())
+}
